8a50ea6c3b218f8460c87182bd6dc99b4825f19c
akimoto <34627500+15534448358@users.noreply.github.com>
2025-11-21 15:24:27 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -41,8 +41,8 @@ public class WechatConfig {
                 "o7P352HnLyJ4eORnSMiEgiQGwdjE", //扫码关注你的测试号以后，测试平台会出现TA的微信号
                 "jvjwBYsqJGYB8RqgFhFjJa8fhQlD4SWT8wODEI2fZgY", //要给这个人发送的模板ID
                 "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
+                "上海市闵行区浦江镇", //这个人的详细地址
+                "浦江", //这个人在的城市
                 new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
                 new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
                 new BirthDay(2020,7,8,true,true),
